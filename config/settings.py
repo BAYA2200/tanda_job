ea646025cbf216a5980cd7e66f2ee30a34969156
BAYA2200 <bayaman2101@gmail.com>
2024-11-23 23:57:22 +0600

    Fix static files for Render 5

--- config/settings.py
+++ config/settings.py
@@ -115,3 +115,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Для корректной обработки статических файлов в продакшене
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
