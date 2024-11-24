bdfbfaf0e770affb53b3c55ab6e0077a8b844d36
BAYA2200 <bayaman2101@gmail.com>
2024-11-24 21:50:12 +0600

    add docs swagger 4

--- config/schema.py
+++ config/schema.py
@@ -5,6 +5,11 @@ from graphene_django import DjangoObjectType
 from orders.schema import Query as OrdersQuery, Mutation as OrdersMutation
 import graphql_jwt
 
+from django.contrib.auth.models import User
+from graphene_django import DjangoObjectType
+from orders.schema import Query as OrdersQuery, Mutation as OrdersMutation
+import graphql_jwt
+from graphql_jwt.shortcuts import get_token, create_refresh_token
 class Query(OrdersQuery, graphene.ObjectType):
     pass
 
@@ -36,6 +41,21 @@ class Mutation(graphene.ObjectType):
     # Мутация для выхода (удаление refresh токена)
     revoke_token = graphql_jwt.Revoke.Field()
 
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+    token = graphene.String()
+    refresh_token = graphene.String()
+
+    class Arguments:
+        username = graphene.String(required=True)
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    def mutate(self, info, username, email, password):
+        user = User.objects.create_user(username=username, email=email, password=password)
+        token = get_token(user)
+        refresh_token = create_refresh_token(user)
+        return CreateUser(user=user, token=token, refresh_token=refresh_token)
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
 
